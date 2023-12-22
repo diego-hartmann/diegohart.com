c0ac1f665ba5b7a5be6f85585d65060ad7cd9185
Diego Hartmann <122852487+diego-hartmann@users.noreply.github.com>
2023-12-22 21:29:13 +0000

    Update index.jsx

--- src/modules/Home/Game/Obstacles/Obstacle/index.jsx
+++ src/modules/Home/Game/Obstacles/Obstacle/index.jsx
@@ -11,24 +11,41 @@ const Obstacle = ({ index }) => {
     }
 
     const _delay = getRandomFloat(index + 2, index + 4, 2);
+    
     setDelay(_delay);
 
-    // Event listener function
     const handleCollision = () => {
-      alert('Player hit the obstacle!');
-      // apply other logics
+      alert('GAME OVER');
+      location.reload();
     };
 
+    // Attach the event listener to the obstacle element
+    const obstacleElement = obstacleRef.current;
+
     const playerElement = document.getElementById('player');
-    if (playerElement) {
-      playerElement.addEventListener('collision', handleCollision);
+    if (playerElement && obstacleElement) {
+      const obstacleRect = obstacleElement.getBoundingClientRect();
+      const playerRect = playerElement.getBoundingClientRect();
+
+      // Check for collision when the component mounts
+      if (isColliding(obstacleRect, playerRect)) {
+        handleCollision();
+      }
     }
     return () => {
-      if (playerElement) {
-        playerElement.removeEventListener('collision', handleCollision);
-      }
+      // apply cleanup
     };
   }, [index]);
+
+  // Function to check for collision between two elements
+  const isColliding = (rect1, rect2) => {
+    return (
+      rect1.x < rect2.x + rect2.width &&
+      rect1.x + rect1.width > rect2.x &&
+      rect1.y < rect2.y + rect2.height &&
+      rect1.y + rect1.height > rect2.y
+    );
+  };
 
   return <div className="obstacle" style={{ animationDelay: `${delay}s` }} ref={obstacleRef} />;
 };
