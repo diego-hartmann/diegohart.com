657561714fe772a63589f946f80de7004f30160b
Diego Hartmann <122852487+diego-hartmann@users.noreply.github.com>
2023-12-22 21:24:07 +0000

    Update index.jsx

--- src/modules/Home/Game/Obstacles/Obstacle/index.jsx
+++ src/modules/Home/Game/Obstacles/Obstacle/index.jsx
@@ -1,16 +1,36 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 
-const Obstacle = ({index}) => {
-    const [delay, setDelay] = useState(1)
-    useEffect(()=>{
-        function getRandomFloat(min, max, decimals) {
-            const str = (Math.random() * (max - min) + min).toFixed(decimals);
-            return parseFloat(str);
-        }
-        const _delay = getRandomFloat(index+2, index+4, 2);
-        setDelay(_delay)
-    },[])
+const Obstacle = ({ index }) => {
+  const [delay, setDelay] = useState(1);
+  const obstacleRef = useRef(null);
 
-    return <div className="obstacle" style={{animationDelay:`${delay}s`}}/>
-}
-export default Obstacle
+  useEffect(() => {
+    function getRandomFloat(min, max, decimals) {
+      const str = (Math.random() * (max - min) + min).toFixed(decimals);
+      return parseFloat(str);
+    }
+
+    const _delay = getRandomFloat(index + 2, index + 4, 2);
+    setDelay(_delay);
+
+    // Event listener function
+    const handleCollision = () => {
+      alert('Player hit the obstacle!');
+      // apply other logics
+    };
+
+    const playerElement = document.getElementById('player');
+    if (playerElement) {
+      playerElement.addEventListener('collision', handleCollision);
+    }
+    return () => {
+      if (playerElement) {
+        playerElement.removeEventListener('collision', handleCollision);
+      }
+    };
+  }, [index]);
+
+  return <div className="obstacle" style={{ animationDelay: `${delay}s` }} ref={obstacleRef} />;
+};
+
+export default Obstacle;
